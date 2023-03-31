c77033d3e4bcfb537f6e288f599a1274489daff6
markov199 <99345543+markov199@users.noreply.github.com>
2023-03-31 13:34:23 +0400

    Update parse_utils.c

--- parse_utils.c
+++ parse_utils.c
@@ -7,17 +7,20 @@ int ft_get_element(char *line, t_map *map)
 {
 	char **split_line;
 	char **colour_split;
+	int element;
+	
+	element = 0;
 
 	split_line = ft_split(line, ' ');
-	if (map->texture_path_north = NULL && !ft_strcmp(split_line[0], "NO "))
+	if (map->texture_path_north = NULL && !ft_strcmp(split_line[0], "NO ") && ++element)
 		map->texture_path_north = split_line[1];
-	else if (map->texture_path_south = NULL && !ft_strcmp(split_line[0], "SO "))
+	else if (map->texture_path_south = NULL && !ft_strcmp(split_line[0], "SO ") && ++element)
 		map->texture_path_south = split_line[1];
-	else if (map->texture_path_west = NULL && !ft_strcmp(split_line[0], "WE "))
+	else if (map->texture_path_west = NULL && !ft_strcmp(split_line[0], "WE ") && ++element)
 		map->texture_path_west = split_line[1];
-	else if (map->texture_path_east = NULL && !ft_strcmp(split_line[0], "EA "))
+	else if (map->texture_path_east = NULL && !ft_strcmp(split_line[0], "EA ") && ++element)
 		map->texture_path_east = split_line[1];
-	else if (!ft_strcmp(split_line[0], "F "))
+	else if (map->floor_colour[0] = -1 && !ft_strcmp(split_line[0], "F ") && ++element)
 	{
 		colour_split = ft_split(split_line[[1], ',')
 		map->floor_colour[0] = colour_split[0]
@@ -25,11 +28,12 @@ int ft_get_element(char *line, t_map *map)
 		map->floor_colour[2] = colour_split[2]			
 	}
 	
-	else if (!ft_strcmp(split_line[0], "C "))
+	else if (map->celing_colour[0] = -1 && !ft_strcmp(split_line[0], "C ") && ++element)
 	{
 		colour_split = ft_split(split_line[[1], ',')
 		map->celing_colour[0] = colour_split[0]
 		map->celing_colour[1] = colour_split[1]				   
 		map->celing_colour[2] = colour_split[2]			
 	}
+	return (element);				
 }
